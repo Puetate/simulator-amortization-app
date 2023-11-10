091e7cacb812fc3e89e0f695e511a2445d0d0d7d
Pablo <jhoelpablo02@gmail.com>
2023-11-10 12:22:12 -0500

    Add Mantine preset to PostCSS and implement registration form UI.

--- src/pages/Register/components/FormRegister.tsx
+++ src/pages/Register/components/FormRegister.tsx
@@ -1,8 +1,81 @@
+import { Box, Button, Text } from "@mantine/core";
+import { Link } from "react-router-dom";
+import { PublicRoutes } from "../../../models/routes";
 
-export default function FormRegister() { 
+export default function FormRegister() {
 
     return (
         <>
+            <Box className="bg-white flex items-center justify-center h-full w-1/3 mx-auto">
+                <div className="sm:mx-auto sm:w-96 sm:max-w-full">
+                    <div className="sm:mx-auto sm:w-96 sm:max-w-full">
+                        {/* <img className="mx-auto h-55 w-40" src={logo} alt="Your Company" /> */}
+                        <Text className="text-center text-2xl font-bold leading-9 tracking-tight text-blue-800">
+                            Sistema de Amortización
+                        </Text>
+                    </div>
+
+                    <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+                        <form className="space-y-6">
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block text-sm font-medium leading-6 text-gray-900"
+                                > Correo Electrónico
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label
+                                        htmlFor="password"
+                                        className="block text-sm font-medium leading-6 text-gray-900"
+                                    >
+                                        Contraseña
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        required
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <Button
+                                    type="submit"
+                                    className="flex w-full justify-center rounded-md bg-blue-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >
+                                    Ingresar
+                                </Button>
+                            </div>
+                        </form>
+
+                        <p className="mt-10 text-center text-sm text-gray-500">
+                            Ya tienes una cuenta?{" "}
+                            <Link
+                                to={PublicRoutes.login}
+                                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                            > Iniciar Sesión!
+                            </Link>
+                        </p>
+                    </div>
+                </div>
+            </Box>
         </>
     );
 }
