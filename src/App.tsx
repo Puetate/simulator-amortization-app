93f54f817c333115a5591e018a3fedd42c0bb903
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 12:11:27 -0500

    feat; import tailwind and mantine styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { MantineProvider } from "@mantine/core";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import "@mantine/core/styles.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PublicRoutes } from "./models/routes";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 
 function App() {
-    return (
-      <MantineProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navigate to="/login" />} />
-            <Route path={PublicRoutes.login} element={<Login />} />
-            {<Route path={PublicRoutes.register} element={<Register />} />}
-          </Routes>
-        </BrowserRouter>
-      </MantineProvider>
-    );
+  return (
+    <MantineProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path={PublicRoutes.login} element={<Login />} />
+          {<Route path={PublicRoutes.register} element={<Register />} />}
+        </Routes>
+      </BrowserRouter>
+    </MantineProvider>
+  );
 }
 
 export default App;
