391d84d0de16118f9472f5fed3412684553fa2fa
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-10 12:14:41 -0500

    feat: add snackbar provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { SnackbarProvider } from "notistack";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PublicRoutes } from "./models/routes";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import { SnackbarManagerConfigurator } from "./utils";
 
 function App() {
   return (
     <MantineProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
-          <Route path={PublicRoutes.login} element={<Login />} />
-          {<Route path={PublicRoutes.register} element={<Register />} />}
-        </Routes>
-      </BrowserRouter>
+      <SnackbarProvider>
+        <SnackbarManagerConfigurator />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path={PublicRoutes.login} element={<Login />} />
+            {<Route path={PublicRoutes.register} element={<Register />} />}
+          </Routes>
+        </BrowserRouter>
+      </SnackbarProvider>
     </MantineProvider>
   );
 }
