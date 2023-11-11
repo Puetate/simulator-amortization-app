3237e5eebb02d1c95d811604950d8eafac0cc152
José Pazmiño <pepe.paz0399@gmail.com>
2023-11-11 03:41:26 -0500

    feat: refactor CheckSession component to handle user roles and default routes

--- src/components/CheckSession.tsx
+++ src/components/CheckSession.tsx
@@ -1,13 +1,22 @@
-// const allowedRoutes = Object.values(PublicRoutes) as string[];
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { AdminRoutes, PublicRoutes, UserRoles, UserRoutes } from "../models";
+import { useSessionStore } from "../store";
+
+const allowedRoutes = Object.values(PublicRoutes) as string[];
+
+const defaultRoutes: Record<string, string> = {
+  ADMIN: AdminRoutes.company,
+  USER: UserRoutes.amortization
+};
 
 export default function CheckSession() {
-  // const { token, user } = useSessionStore();
-  // const location = useLocation();
-  // return token === "" || user.roles !== UserRoles.ADMIN ? (
-  // 	<Outlet />
-  // ) : allowedRoutes.includes(location.pathname) ? (
-  // 	<Navigate to={AppRoutes.SEND_ROUTINES} />
-  // ) : (
-  // 	<Navigate to={PublicRoutes.DEFAULT} />
-  // );
+  const { token, user } = useSessionStore();
+  const location = useLocation();
+  return token === "" || user.roles[0].name !== UserRoles.UNDEFINED ? (
+    <Outlet />
+  ) : allowedRoutes.includes(location.pathname) ? (
+    <Navigate to={defaultRoutes[user.roles[0].name]} />
+  ) : (
+    <Navigate to={PublicRoutes.default} />
+  );
 }
